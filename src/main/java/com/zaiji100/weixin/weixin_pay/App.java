a46c8f1f07422367e2670e8b29d1ddc295bda191
zaiji100 <1076421128@qq.com>
2015-04-24 15:07:27 +0800

    初始化项目

---
+++ src/main/java/com/zaiji100/weixin/weixin_pay/App.java
@@ -0,0 +1,13 @@
+package com.zaiji100.weixin.weixin_pay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
